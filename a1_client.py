f52e147d30eca7f75e57f64488d530be8bc7090c
Ryland Whillans <ryland.whillans@gmail.com>
2019-09-23 22:28:20 -0400

    Working test

--- a1_client.py
+++ a1_client.py
@@ -1,6 +1,6 @@
 from socket import *
 lokiAddr = "192.197.151.116"
-serverPort = "50330"
+serverPort = 50330
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((lokiAddr, serverPort))
 clientSocket.send("toast".encode())
--- a1_server.py
+++ a1_server.py
@@ -1,10 +1,10 @@
 from socket import *
-serverPort = "50330"
+serverPort = 50330
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(("", serverPort))
 serverSocket.listen(1)
 while True:
     connectionSocket, addr = serverSocket.accept()
-    packet = connectionSocket.recv(512).decode
-    connectionSocket.send(packet.upper().encode)
+    packet = connectionSocket.recv(512).decode()
+    connectionSocket.send(packet.upper().encode())
     connectionSocket.close()
