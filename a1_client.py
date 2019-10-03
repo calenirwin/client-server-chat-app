b484bf85880afe5750ef78e1e1adbe4c1c98cb22
Calen Irwin <48806102+calenirwin@users.noreply.github.com>
2019-10-03 16:45:32 -0400

    Added packet definition and select logic for getting readable sockets

--- a1_client.py
+++ a1_client.py
@@ -1,6 +1,20 @@
+# Name: a1_client.py
+# Made for COIS-4310H Assignment 1
+# Author: Calen Irwin [0630330] & Ryland Whillens [0618437]
+# Purpose: Client portion of Client/Server chat application
+# References: https://www.pubnub.com/blog/socket-programming-in-python-client-server-p2p/
+#             https://www.geeksforgeeks.org/socket-programming-multi-threading-python/
+#             https://www.binarytides.com/code-chat-application-server-client-sockets-python/
 from socket import *
 from struct import *
 VERSION = "1"
+
+# Packet Definition
+# ! = byte-ordering
+# H = unsigned short
+# c = character
+# p = varaible length string where the maximum length is specified by the number
+#     proceeding it minus 1 (e.g. 21p is a string of maximum 20 characters)
 packetStruct = Struct("!Hc21p21p11p256p")
 packetNum = 0
 
@@ -9,37 +23,52 @@ serverPort = 50330
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((lokiAddr, serverPort))
 # todo: get username/confirm
-# todo: get input/ select
-userInput = userInput.split(":", 1) 
-if len(usrInput) == 2:
-    if (len(usrInput[1]) > 255):
-        print("Message too long")
-    else: 
-        if usrInput[0] == "all":
-            pkt = packetStruct.pack(VERSION, packetNum, user, "all", "msgAll", usrInput[1])
-            clientSocket.send(pkt)
-            packetNum += 1
-        else: 
-            if (len(usrInput[0]) > 20 or not usrInput[0]):
-                print("Invalid username")
+while True:
+    # standard input socket and client socket
+    socketList = [sys.stdin, clientSocket]
+    # use select to get sockets ready to be read
+    readSockets, writeSockets, errorSockets = select.select(socketList, [], [])
+    # loop through readable sockets
+    for s in readSockets:
+        # if the socket is the client socket, then the client is being sent
+        # data from the server
+        # TODO: add proper packet handling
+        if s == clientSocket:
+            data = s.recv(1024)
+            if not data:
+                print('\nTerminanting chat room connection.')
+                sys.exit()
             else:
-                pkt = packetStruct.pack(VERSION, packetNum, user, userInput[0], "msg", usrInput[1])
+                sys.stdout.write(data)
+        # otherwise, the client has written in the console
+        else:
+            # read input from client
+            userInput = sys.stdin.readline()
+            userInput = userInput.split(":", 1)
+
+            if len(usrInput) == 2:
+                if (len(usrInput[1]) > 255):
+                    print("Message too long")
+                else:
+                    if usrInput[0] == "all":
+                        pkt = packetStruct.pack(VERSION, packetNum, user, "all", "msgAll", usrInput[1])
+                        clientSocket.send(pkt)
+                        packetNum += 1
+                    else:
+                        if (len(usrInput[0]) > 20 or not usrInput[0]):
+                            print("Invalid username")
+                        else:
+                            pkt = packetStruct.pack(VERSION, packetNum, user, userInput[0], "msg", usrInput[1])
+                            clientSocket.send(pkt)
+                            packetNum += 1
+            elif usrInput == "who":
+                pkt = packetStruct.pack(VERSION, packetNum, user, "", "who", "")
                 clientSocket.send(pkt)
                 packetNum += 1
-elif usrInput == "who":
-    pkt = packetStruct.pack(VERSION, packetNum, user, "", "who", "")
-    clientSocket.send(pkt)
-    packetNum += 1
-elif  usrInput == "bye":
-    pkt = packetStruct.pack(VERSION, packetNum, user, "", "bye", "")
-    clientSocket.send(pkt)
-    clientSocket.close()
-
-else:
-    print("Unknown Command")
-
-
+            elif  usrInput == "bye":
+                pkt = packetStruct.pack(VERSION, packetNum, user, "", "bye", "")
+                clientSocket.send(pkt)
+                clientSocket.close()
+            else:
+                print("Unknown Command")
 clientSocket.close()
-
-
-
